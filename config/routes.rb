3ea70ac13a2707a1fa933ac1788a0e346faade7f
yunhao17 <yunhao17@gmail.com>
2021-06-03 07:47:22 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
  
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update]do
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+ end
  
 end
