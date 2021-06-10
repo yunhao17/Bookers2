9e64cf524c065828e35e2012ee0294015bcbba0a
yunhao17 <yunhao17@gmail.com>
2021-06-10 05:23:19 +0000

    Finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_books, through: :favorites, source: :book
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
   
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
